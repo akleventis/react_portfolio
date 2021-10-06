f102710e9ffb8d8bc6d95cd89f6d2c1409b860a0
Alex Leventis <alex.leventis1@gmail.com>
2021-10-06 16:31:24 -0400

    github image in coding component

--- src/DynamicDiv/Code.js
+++ src/DynamicDiv/Code.js
@@ -1,25 +1,26 @@
-import React from "react"
+import React from "react";
 
-import computer from "../Images/computer.jpg"
+import computer from "../Images/computer.jpg";
+import github from "../Images/github.png"
 
-import "./dynamicDiv.css"
-
+import "./dynamicDiv.css";
 
 class Computer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
-
-
-    render () {
-        return( 
-        <img className="my_image_click" src = {computer} alt="computer"/>
-      )
-    }
+  render() {
+    return (
+      <div className="code_container">
+        <a  className="github" href="http://alexcod.es/github" target="_blank" rel="noreferrer">
+            <img className="git_image" src={github} alt="github" />
+        </a>
+        <img className="my_image_click" src={computer} alt="computer" />
+      </div>
+    );
+  }
 }
 
-
-export default Computer
+export default Computer;
